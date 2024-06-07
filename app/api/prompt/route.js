4bd8f2410c80b46d4519c34d60a17f38b75027b2
Rohan Paul <rohanpaul8000@gmail.com>
2024-06-07 13:54:26 +0530

    Auth

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,27 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
+// export const GET = async (request) => {
+//     try {
+//         await connectToDB()
+//         const prompts = await Prompt.find({}).populate('creator')
+//         return new Response(JSON.stringify(prompts), { status: 200 })
+//     } catch (error) {
+//         console.log(error);
+//         return new Response("Failed to fetch all prompts", { status: 500 })
+//     }
+// } 
+
 export const GET = async (request) => {
+    console.log(request);
     try {
         await connectToDB()
-        const prompts = await Prompt.find({}).populate('creator')
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        const prompt = await Prompt.find({}).populate('creator')
+        if (!prompt) return new Response("Prompt not found", { status: 404 })
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
     } catch (error) {
         console.log(error);
-        return new Response("Failed to fetch all prompts", { status: 500 })
+        return new Response("Failed to fetch prompt", { status: 500 })
     }
-} 
+}
